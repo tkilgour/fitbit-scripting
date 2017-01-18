364a2b71941109a3433be5d611b307cc03ee3369
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-18 14:52:33 -0500

    working basic API request

--- fitbit.js
+++ fitbit.js
@@ -1,8 +1,24 @@
 require('dotenv').config();
-var request = require("request");
+const request = require("request");
 
 // FitBit Authorization – Please create a .env with the following two lines:
 // FITBIT_USER = <Your Username>
 // FITBIT_TOKEN = <Your Token>  ***Expires on FEB 16, 2017***
-var FITBIT_USER = process.env.FITBIT_USER;
-var FITBIT_TOKEN = process.env.FITBIT_TOKEN;
+const FITBIT_USER = process.env.FITBIT_USER;
+const FITBIT_TOKEN = process.env.FITBIT_TOKEN;
+
+let options = {
+  url: 'https://api.fitbit.com/1/user/-/profile.json',
+  headers: {
+    Authorization: `Bearer ${FITBIT_TOKEN}`
+  }
+};
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var data = JSON.parse(body);
+    console.log(data);
+  }
+}
+
+request(options, callback);
